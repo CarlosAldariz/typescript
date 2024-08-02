da710627f5abac528ce8c755d6c2a85847d04ef1
Carlos Aldariz <carlosaldariz@hotmail.com>
2024-08-02 15:46:04 +0200

    añadidos arrais basicos

---
+++ bases/tipos/array.ts
@@ -0,0 +1,14 @@
+(() => { 
+
+    const numbers: (string | number | boolean)[] = [1,2,3,4,5,'6',7,8,9,]; 
+
+    numbers.push(true); 
+
+    console.log(numbers) 
+
+    const villains =  ["redskull", "wolf"]; 
+
+    villains.forEach( v => console.log(v.toUpperCase()))
+
+})() 
+
