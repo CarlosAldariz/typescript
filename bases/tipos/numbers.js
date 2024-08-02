7e61d6881d827abd65a0810601997295ce77b10f
Carlos Aldariz <carlosaldariz@hotmail.com>
2024-08-02 15:08:23 +0200

    añadimos numeros

---
+++ bases/tipos/numbers.js
@@ -0,0 +1,13 @@
+"use strict";
+(() => {
+    let avengers = 10;
+    console.log(avengers);
+    const villains = 20;
+    if (avengers < villains) {
+        console.log("problemas");
+    }
+    else {
+        console.log("no hay problemas");
+    }
+    //en javascript NaN es considerado del tipo number
+})();
---
+++ bases/tipos/numbers.ts
@@ -0,0 +1,17 @@
+(()=>{ 
+
+let avengers: number = 10; 
+
+console.log (avengers) 
+
+const villains: number = 20; 
+
+if (avengers < villains) {
+    console.log("problemas")
+} else { 
+    console.log("no hay problemas")
+} 
+
+//en javascript NaN es considerado del tipo number
+
+} )()
