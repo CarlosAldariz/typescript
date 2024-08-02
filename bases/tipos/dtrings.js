b6a9504be40ffa0c8c5e6b833334ebfb60788c07
Carlos Aldariz <carlosaldariz@hotmail.com>
2024-08-02 15:22:26 +0200

    añado strings

---
+++ bases/tipos/dtrings.js
@@ -0,0 +1,11 @@
+"use strict";
+(() => {
+    var _a;
+    const batman = 'Batman';
+    const linternaVerde = "Linterna Verde";
+    const superman = `Superman`;
+    console.log(` I'm ${batman}`);
+    console.log(batman.toUpperCase());
+    console.log(batman.toLowerCase());
+    console.log(((_a = batman[10]) === null || _a === void 0 ? void 0 : _a.toUpperCase()) || 'No esta presente');
+});
---
+++ bases/tipos/dtrings.ts
@@ -0,0 +1,13 @@
+(() => { 
+
+const batman: string = 'Batman';  
+const linternaVerde: string = "Linterna Verde"; 
+const superman: string = `Superman`; 
+
+console.log(` I'm ${batman}`); 
+console.log(batman.toUpperCase()); 
+console.log(batman.toLowerCase()); 
+console.log(batman[10]?.toUpperCase() || 'No esta presente')
+
+}
+)
