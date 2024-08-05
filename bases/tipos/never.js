9fce0d42d8470e745ada7dddabc06d81202e9608
Carlos Aldariz <carlosaldariz@hotmail.com>
2024-08-05 08:35:54 +0200

    varios exercicios simples añadidos

---
+++ bases/tipos/never.js
@@ -0,0 +1,11 @@
+"use strict";
+(() => {
+    const error = (message) => {
+        if (false) {
+            throw new Error(message);
+        }
+        return 1;
+    };
+    error('Mensaje de error');
+    console.log('ola Mundo');
+})();
---
+++ bases/tipos/null&undefined.ts
@@ -0,0 +1,11 @@
+(() => { 
+
+    let nada:(undefined|boolean) = undefined; 
+
+    console.log(nada);  
+
+    let nada1:(null|undefined|boolean) = null; 
+    
+    console.log(nada1);
+
+})() 
