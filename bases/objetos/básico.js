642bdc2e200db0a543070fcb854214db22cd5a2d
Carlos Aldariz <carlosaldariz@hotmail.com>
2024-08-05 09:53:14 +0200

    basico objetos

---
+++ bases/objetos/básico.js
@@ -0,0 +1,13 @@
+"use strict";
+(() => {
+    let identidad = {
+        name: 'Carlos',
+        age: 32,
+        capacidades: ['informatica', 'bodyboard']
+    };
+    identidad = {
+        name: 'Carlos2',
+        age: 60,
+        capacidades: ['rata de biblioteca']
+    };
+})();
---
+++ bases/objetos/básico.ts
@@ -0,0 +1,15 @@
+(() => { 
+
+    let identidad = { 
+        name: 'Carlos', 
+        age: 32,  
+        capacidades: ['informatica', 'bodyboard']
+    }
+
+    identidad = { 
+        name: 'Carlos2', 
+        age: 60, 
+        capacidades: ['rata de biblioteca']
+    }
+
+})() 
