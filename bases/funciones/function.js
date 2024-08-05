1e0285f54fa1db259324ba4cdc6d7e6b4da59f9c
Carlos Aldariz <carlosaldariz@hotmail.com>
2024-08-05 08:46:57 +0200

    funciones añadido

---
+++ bases/funciones/function.js
@@ -0,0 +1,11 @@
+"use strict";
+(() => {
+    const nombre = 'Carlos';
+    function returnName() {
+        return 'nombre';
+    }
+    const mensaje = () => {
+        return 'mensaje enviado';
+    };
+    console.log(typeof mensaje);
+})();
---
+++ bases/funciones/function.ts
@@ -0,0 +1,15 @@
+(() => { 
+
+    const nombre: string = 'Carlos'; 
+
+    function returnName():string { 
+        return 'nombre';
+        } 
+
+        const mensaje = ():string => {
+            return 'mensaje enviado';
+        } 
+
+        console.log(typeof mensaje); 
+
+})() 
