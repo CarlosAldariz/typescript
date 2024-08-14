704eeffe9697cec6b14930c3734e7d3de141a049
Carlos Aldariz <carlosaldariz@hotmail.com>
2024-08-14 13:19:59 +0200

    extends.ts

--- bases/clases/basica.ts
+++ bases/clases/basica.ts
@@ -3,7 +3,7 @@
     class Avenger {
         private name: string;
         public team: string;
-        public realname: string;
+        public realname?: string;
     
         constructor(name: string, team: string, realname: string) {
             this.name = name;
---
+++ bases/clases/extends.ts
@@ -0,0 +1,28 @@
+class Avenger { 
+    constructor( 
+        public name: string, 
+        public realName: string, 
+    ) {
+    console.log('Constructor Avenger llamado!'); 
+    } 
+
+    private getFullName() { 
+        return `${ this.name } ${ this.realName} `
+    }
+
+} 
+
+class Xmen extends Avenger { 
+
+    constructor(  
+        name: string, 
+        realName: string,
+        public isMutant:boolean
+    ){ 
+        super( name, realName ) 
+        console.log('constructor xmen llamado');
+    }
+
+} 
+
+const wolverine = new Xmen('wolverine', 'Logan', true);
