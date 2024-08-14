e065f73b6fc0cb054409585fd3446bab65cd0e0f
Carlos Aldariz <carlosaldariz@hotmail.com>
2024-08-14 12:40:29 +0200

    añadimos clases

---
+++ bases/clases/basica.ts
@@ -0,0 +1,17 @@
+(() => { 
+
+    class Avenger {
+        private name: string;
+        public team: string;
+        public realname: string;
+    
+        constructor(name: string, team: string, realname: string) {
+            this.name = name;
+            this.team = team;
+            this.realname = realname;
+        }
+    }
+    
+    const antman: Avenger = new Avenger("Ant-Man", "Avengers", "Scott Lang");
+
+})() 
